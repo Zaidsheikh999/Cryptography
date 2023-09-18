5f208835cee7b99c9108303035d81847673cb3ae
Zaid Sheikh <zaid.shaikh@cubixlabs.com>
2023-09-18 18:52:06 +0500

    Cryptography

---
+++ Cryptography/View/CryptoView.swift
@@ -0,0 +1,73 @@
+//
+//  CryptoView.swift
+//  Cryptography
+//
+//  Created by Zaid Sheikh on 18/09/2023.
+//
+
+import SwiftUI
+import UIKit
+
+struct CryptoView: View {
+    
+    @ObservedObject var viewModel = CryptoViewModel()
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Input")) {
+                TextField("Text to encrypt", text: $viewModel.input)
+            }
+            
+            Section(header: Text("Encryption Key")) {
+                TextField("Key (16 chars)", text: $viewModel.key)
+                    .textContentType(.password)
+            }
+            
+            Section {
+                Button("Encrypt") {
+                    viewModel.encrypt()
+                }
+                
+                Button("Decrypt") {
+                    viewModel.decrypt()
+                }
+            }
+            
+            Section(header: Text("Encrypted Text")) {
+                Text(viewModel.encryptedText)
+                
+                if !viewModel.encryptedText.isEmpty{
+                    Button("Copy Encrypted Text") {
+                        UIPasteboard.general.string = viewModel.encryptedText
+                    }
+                }
+            }
+            
+            Section(header: Text("Decrypted Text")) {
+                Text(viewModel.decryptedText)
+                
+                if !viewModel.decryptedText.isEmpty{
+                    Button("Copy Decrypted Text") {
+                        UIPasteboard.general.string = viewModel.decryptedText
+                    }
+                    
+                }
+            }
+            
+            if !viewModel.error.isEmpty {
+                Section {
+                    Text("Error: \(viewModel.error)")
+                        .foregroundColor(.red)
+                }
+            }
+        }
+        
+    }
+}
+
+struct CryptoView_Previews: PreviewProvider {
+    static var previews: some View {
+        CryptoView()
+    }
+}
+
---
+++ Cryptography/ViewModel/CryptoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CryptoViewModel.swift
+//  Cryptography
+//
+//  Created by Zaid Sheikh on 18/09/2023.
+//
+
+import Foundation
