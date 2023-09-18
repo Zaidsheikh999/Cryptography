f783840cbd87573aced16032ac9e37c67ad49e33
Zaid Sheikh <zaid.shaikh@cubixlabs.com>
2023-09-18 17:42:12 +0500

    Initial Commit

---
+++ Cryptography/CryptographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptographyApp.swift
+//  Cryptography
+//
+//  Created by Zaid Sheikh on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
